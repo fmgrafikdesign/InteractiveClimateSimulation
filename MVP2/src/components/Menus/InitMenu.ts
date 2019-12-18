cd6dde05672ae4f71879cb0f75c36cba3160d049
Mueggrogondrolla <S1610238045@students.fh-hagenberg.at>
2019-12-18 18:20:56 +0100

    first basic map click to generate terrain functionality

--- MVP2/src/components/Menus/InitMenu.ts
+++ MVP2/src/components/Menus/InitMenu.ts
@@ -1,40 +1,54 @@
 import m from 'mithril';
 import Nav from '../Nav';
 import {options} from "../Terrain/Generators/GeneratorOptions/GeneratorOptions";
+import * as mapBox from 'mapbox-gl';
+import {MapLayerMouseEvent} from "mapbox-gl";
 
 const items: Array<m.Vnode<any, any>> = generateMenuItems();
 
 
 const App: m.Component = {
-	view: () =>
-		m('.page', {onclick: testFunction},
-			m(Nav),
-			m("h1.Init-Menu-Headline", "Choose an option how the simulated terrain is being generated"),
-			m("div.Init-Menu-Items-Container", items)
-		)
+    oncreate: ({dom}) => {
+        const map = new mapBox.Map({
+            container: 'map', // Specify the container ID
+            style: 'mapbox://styles/mapbox/outdoors-v11', // Specify which map style to use
+            center: [14.515, 48.368], // Specify the starting position [lng, lat]
+            zoom: 15.5, // Specify the starting zoom
+            accessToken: 'pk.eyJ1IjoibXVlZ2dyb2dvbmRyb2xsYSIsImEiOiJjazJ4ZjEyY2kwYjBzM2dvbWVoYjF4MndnIn0.CkaTPt0RObUkNLIUiWSecg'
+        });
+		map.on("click", testFunction);
+    },
+    view: () =>
+        m('.page',
+            m(Nav),
+            m("h1.Init-Menu-Headline", "Choose an option how the simulated terrain is being generated"),
+            m("div.Init-Menu-Items-Container", items),
+            //m("div.Map-Container", m("div#map"))
+        )
 };
 
-function generateMenuItems(): Array<m.Vnode<any, any>>
-{
-	const generatedItems: Array<m.Vnode<any, any>> = [];
+function generateMenuItems(): Array<m.Vnode<any, any>> {
+    const generatedItems: Array<m.Vnode<any, any>> = [];
 
-	for (let i = 0; i < options.length; i++)
-	{
-		let itemID: string = "MenuItem" + i;
-		generatedItems.push(
-			m("div.Init-Menu-Item-Wrapper",
-				//m(m.route.Link, {href: options[i].link}, // TODO: add the chosen type as a parameter
-				m("div", {onclick: testFunction, id: itemID, class: "Init-Menu-Item", link: options[i].link, linkParameter: options[i].type},
-					m("h3", options[i].name))));
-	}
+    for (let i = 0; i < options.length; i++) {
+        let itemID: string = "MenuItem" + i;
+        generatedItems.push(
+            m("div.Init-Menu-Item-Wrapper",
+                //m(m.route.Link, {href: options[i].link}, // TODO: add the chosen type as a parameter
+                m("div", {onclick: testFunction, id: itemID, class: "Init-Menu-Item", link: options[i].link, linkParameter: options[i].type},
+                    m("h3", options[i].name))));
+    }
 
-	return generatedItems;
+    return generatedItems;
 }
 
-function testFunction(mouseEvent: MouseEvent)
-{
+function testFunction(mouseEvent: MapLayerMouseEvent) {
+    const lng = mouseEvent.lngLat.lng;
+    const lat = mouseEvent.lngLat.lat;
+    console.log(lng + ', ' + lat);
+
     console.log(mouseEvent);
-    m.route.set("/level");
+    //m.route.set("/level");
 
     /*
 	console.log(mouseEvent);
@@ -59,17 +73,14 @@ function testFunction(mouseEvent: MouseEvent)
 	*/
 }
 
-function removeSelectedPropertyOfAllItems()
-{
-	const items: Array<Element> = document.getElementsByClassName("Selected") as unknown as Array<Element>;
+function removeSelectedPropertyOfAllItems() {
+    const items: Array<Element> = document.getElementsByClassName("Selected") as unknown as Array<Element>;
 
-	for (const item of items)
-	{
-		if (item.classList.contains("Selected"))
-		{
-			item.classList.remove("Selected");
-		}
-	}
+    for (const item of items) {
+        if (item.classList.contains("Selected")) {
+            item.classList.remove("Selected");
+        }
+    }
 }
 
 export default App;
--- MVP2/src/components/Terrain/Mainframe.ts
+++ MVP2/src/components/Terrain/Mainframe.ts
@@ -1,6 +1,8 @@
 import m from "mithril"
 import TerrainRenderer, {TerrainRendererInterface} from "./TerrainRenderer";
 import StaticTerrainRenderer from "./StaticTerrainRenderer";
+import * as mapBox from "mapbox-gl";
+import {MapLayerMouseEvent} from "mapbox-gl";
 
 export default function App(): m.Component {
     let container: HTMLElement | undefined;
@@ -11,14 +13,32 @@ export default function App(): m.Component {
         StaticTerrainRenderer.generateTerrainWithLatLng(lat, lng);
     }
 
+    function clickOnMap(mouseEvent: MapLayerMouseEvent) {
+        generateTerrainWithLatLng(mouseEvent.lngLat.lat, mouseEvent.lngLat.lng);
+        const map = document.getElementById("mapWrapper") as HTMLElement;
+        map.classList.add("Map-minimized");
+    }
+
     return {
         oncreate({dom}) {
+            const map = new mapBox.Map({
+                container: 'map', // Specify the container ID
+                style: 'mapbox://styles/mapbox/outdoors-v11', // Specify which map style to use
+                center: [14.515, 48.368], // Specify the starting position [lng, lat]
+                zoom: 15.5, // Specify the starting zoom
+                accessToken: 'pk.eyJ1IjoibXVlZ2dyb2dvbmRyb2xsYSIsImEiOiJjazJ4ZjEyY2kwYjBzM2dvbWVoYjF4MndnIn0.CkaTPt0RObUkNLIUiWSecg'
+            });
+            map.on("click", clickOnMap);
+
+
             container = dom as HTMLElement;
             canvas = container.querySelector('canvas#map-canvas') as HTMLCanvasElement;
 
             // renderer = new TerrainRenderer(canvas);
             //renderer = StaticTerrainRenderer;
             StaticTerrainRenderer.init(canvas);
+
+
 
             // TODO Create resize event listener
         },
@@ -27,15 +47,16 @@ export default function App(): m.Component {
             // TODO remove resize event listener
         },
 
-        view()  {
+        view() {
             return m('.app',
                 [
+                    m('#mapWrapper', m('div#map')),
                     m('.canvas-wrapper', m('canvas#map-canvas')),
                     m('.ui', [
                         // Arbitrary coordinates
-                        m('.change-map-button', {onclick: () => generateTerrainWithLatLng(14.565, 48.378) } ,'Change Map')
+                        m('.change-map-button', {onclick: () => generateTerrainWithLatLng(14.565, 48.378)}, 'Change Map')
                     ])
                 ]);
-    }
+        }
     }
 }
